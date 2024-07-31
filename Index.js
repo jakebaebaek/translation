ac19f6fe047f4710cf8bbdcd689b3c893158dbf4
jake <jacobaek0515@daum.net>
2024-07-31 18:17:31 +0900

    update Readme file

--- Index.js
+++ Index.js
@@ -1,8 +1,11 @@
+
 let symptoms_ul = document.querySelector('.symptoms ul');
 let inputValue = document.querySelector('.sympInput');
-const symptoms_btn = document.querySelector('.CCB');
 const deleteBTN = document.querySelectorAll('#delete');
 const closeBtn = document.querySelector('.windowCloseBtn');
+const { spawn } = require('child_process');
+
+spawn('py',['./googleSide.py']);
 
 deleteBTN.forEach(e => e.innerHTML='❌')
 function paintInput (e) {
@@ -27,6 +30,20 @@ function validate(data) {
     return data;
 }
 
-closeBtn.onclick = function (event) {
-    window.close();
-}
+document.addEventListener("DOMContentLoaded", function() {
+    // 웹 페이지가 로드될 때 Python 코드를 실행
+    const pythonProcess = spawn('python', ['C:\Users\HillsDT12\dev\translation_DangJiN\googleSide.py']);
+
+    pythonProcess.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+        document.getElementById('result').innerText = data;
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    pythonProcess.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+});
